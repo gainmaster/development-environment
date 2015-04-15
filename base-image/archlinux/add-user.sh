d93b51524c8fe38dc1d0fbc479755a9bb1276c33
Tony Hesjevik <tony@hesjevik.no>
2015-04-15 13:33:04 +0200

    Fix sudo

--- base-image/archlinux/add-user.sh
+++ base-image/archlinux/add-user.sh
@@ -5,12 +5,6 @@
 username="$1"
 shell="$2"
 
-if [[ -z $(which sudo) ]]; then
-    echo "Installing sudo"
-    pacman-install sudo
-    echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-fi
-
 useradd -G wheel -s $shell -U $username && passwd -d $username
 mkdir /home/$username
 chown $username:$username /home/$username 
